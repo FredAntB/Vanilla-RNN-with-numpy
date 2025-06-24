2c9498a28474ae03030819591f21f291cff64b56
mauri-antezana <165099222+mauri-antezana@users.noreply.github.com>
2025-06-24 16:47:11 -0400

    Add .gitignore and initial data preprocessing script

---
+++ data_preprocessing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import requests
+
+# Descargar el dataset
+url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+text = requests.get(url).text
+
+# Crear vocabulario
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# Mapeos
+char_to_ix = { ch:i for i,ch in enumerate(chars) }
+ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+# Convertir un caracter a un vector one-hot
+def char_to_one_hot(char_idx, vocab_size):
+    vec = np.zeros((vocab_size, 1))
+    vec[char_idx] = 1
+    return vec
